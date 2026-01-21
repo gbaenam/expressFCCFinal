bf44aeb9c70cf5a06698f292e5efa7da7e50e321
German Baena <germanb66@gmail.com>
2026-01-21 12:45:49 -0500

    Se incluyó la línea 'const { matematicas } = require('../datos/cursos').infoCursos' en matematicas.js

--- routers/matematicas.js
+++ routers/matematicas.js
@@ -1,16 +1,18 @@
 
 const express = require('express')
 
+const { matematicas } = require('../datos/cursos').infoCursos
+
 const routerMatematicas = express.Router()
 
 routerMatematicas.get("/", (req, res) => {
-    res.send(JSON.stringify(infoCursos.matematicas))
+    res.send(JSON.stringify(matematicas))
 })
 
 // Parámetros de ruta cursos de matemáticas
 routerMatematicas.get("/:tema", (req, res) => {
     const tema = req.params.tema
-    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema)
+    const resultados = matematicas.filter(curso => curso.tema === tema)
 
     if (resultados.length === 0) {
         return res.status(404).send(`No encontraron cursos de ${tema}`)
@@ -23,7 +25,7 @@ routerMatematicas.get("/:tema/:nivel", (req, res) => {
     // console.log(req.params)
     const tema = req.params.tema
     const nivel = req.params.nivel
-    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema && curso.nivel === nivel)
+    const resultados = matematicas.filter(curso => curso.tema === tema && curso.nivel === nivel)
 
     if (resultados.length === 0) {
         return res.status(404).send(`No encontraron cursos de ${tema} de nivel ${nivel}`)
